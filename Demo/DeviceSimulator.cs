6558e3cde1005b452094c27ed45787b734f564b3
Felix <56971417+Zerthz@users.noreply.github.com>
2022-10-18 20:05:58 +0200

    Commit made from VS Code Azure Static Web Apps

---
+++ Demo/DeviceSimulator.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public class DeviceSimulator
+    {
+        private readonly ILogger _logger;
+
+        public DeviceSimulator(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DeviceSimulator>();
+        }
+
+        [Function("DeviceSimulator")]
+        [QueueOutput("temperature-reports", Connection = "AzureWebJobsStorage")]
+        public NewTemperatureReading Run([TimerTrigger("*/30 * * * * *")] MyInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var rng = new Random();
+            var reading = new NewTemperatureReading("1", DateTimeOffset.UtcNow, rng.NextDouble() * 100);
+            return reading;
+        }
+    }
+
+    public record NewTemperatureReading(string DeviceId, DateTimeOffset TimeStamp, double Temperature);
+    public class MyInfo
+    {
+        public bool IsPastDue { get; set; }
+    }
+
+}
